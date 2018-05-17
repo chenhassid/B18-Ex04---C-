af9a17e0eed2ba574732ddc647ffa28795fed5d5
Chen Hassid <chenhassid@gmail.com>
2018-05-18 01:11:48 +0300

    Add files via upload

---
+++ B18 Ex04 Matan 312541816 Chen 315473827/Ex04.Menus. Interfaces/MenuItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Interfaces
+{
+    public abstract class MenuItem
+    {
+        private int m_MenuItemOptionNumber = 0;
+        private string m_Title;
+
+        internal abstract void ExecuteMenuOption();
+
+        internal int MenuItemOptionNumber
+        {
+            get
+            {
+                return m_MenuItemOptionNumber;
+            }
+            set
+            {
+                m_MenuItemOptionNumber = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return m_Title;
+            }
+
+            set
+            {
+                m_Title = value;
+            }
+        }
+    }
+}
